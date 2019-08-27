4ebba16f298dcdea0cc8c51c58999611cfa6a331
PuranJoshi <puran@technogise>
2019-08-27 19:52:31 +0530

    Added test case for Junior saver bundle

--- test/product-recommendation.test.js
+++ test/product-recommendation.test.js
@@ -6,6 +6,13 @@ describe('ProductRecommendation', ()=> {
     it('should return return bundle with value 0',() => {
       let bundle = ProductRecommendation.getBundles();
       expect(bundle.value).to.be.equal(0);
+    });
+
+    it('should return return Junior Saver bundle when age is less than 18',() => {
+      let bundle = ProductRecommendation.getBundles(18);
+      expect(bundle.name).to.be.equal('Junior Saver');
+      expect(bundle.value).to.be.equal(0);
     })
+
   })
 })
