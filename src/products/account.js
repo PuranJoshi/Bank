d4f338206a38b9494553e00a99b1b1f03a548ea9
PuranJoshi <puran@technogise>
2019-08-29 13:12:58 +0530

    Extracted method for better reading

--- src/products/account.js
+++ src/products/account.js
@@ -3,27 +3,46 @@ export default class Account {
     this.isStudent = isStudent
     this.type = type
     this.age = age,
-    this.income = income
+      this.income = income
   }
 
   isEligible(customer) {
-    
-    if(this.isStudent != customer.isStudent)
+
+    if (this.isStudent != customer.isStudent)
       return false
 
-    if(this.age)
-    {
-      if (this.age.min && this.age.min >= customer.age)
-        return false;
-      if (this.age.max && this.age.max <= customer.age)
-        return false;
+    if (this.age && this.checkAge(customer.age) === false) {
+      return false;
     }
 
-    if (this.income)
-    { 
-      if(this.income.min && this.income.min >= customer.income)
-        return false;
+    if (this.income && this.checkIncome(customer.income) === false){
+      return false;
     }
+
+    return true;
+  }
+
+  checkAge(age) {
+
+    if (!this.age)
+      return true;
+
+    if (this.age.min && this.age.min >= age)
+      return false;
+    if (this.age.max && this.age.max <= age)
+      return false;
+
+    return true;
+  }
+
+  checkIncome(income) {
+
+    if (!this.income)
+      return true;
+
+    if (this.income.min && this.income.min >= income)
+      return false;
+
     return true;
   }
 }
--- src/products/credit-card.js
+++ src/products/credit-card.js
@@ -6,19 +6,37 @@ export default class CreditCard {
   }
 
   isEligible(customer) {
-    if(this.age)
-    {
-      if (this.age.min && this.age.min >= customer.age)
-        return false;
-      if (this.age.max && this.age.max <= customer.age)
-        return false;
+    if (this.age && this.checkAge(customer.age) === false) {
+      return false;
     }
 
-    if (this.income)
-    { 
-      if(this.income.min && this.income.min >= customer.income)
-        return false;
+    if (this.income && this.checkIncome(customer.income) === false){
+      return false;
     }
+    
+    return true;
+  }
+
+  checkAge(age) {
+
+    if (!this.age)
+      return true;
+
+    if (this.age.min && this.age.min >= age)
+      return false;
+    if (this.age.max && this.age.max <= age)
+      return false;
+
+    return true;
+  }
+
+  checkIncome(income) {
+
+    if (!this.income)
+      return true;
+
+    if (this.income.min && this.income.min >= income)
+      return false;
 
     return true;
   }
